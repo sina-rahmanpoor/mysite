30012740971306eb0b5bf0b538ddc974ff8441c8
sina <sina1386sollyx@gmail.com>
2023-12-21 00:19:54 +0330

    GI
    WAD

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from website.url import http_test,json_test
+from website.urls import urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path ('url address','view')
-    path('website/',include('website.urls'))
+    path('website/',include(urlpatterns))
 ]
